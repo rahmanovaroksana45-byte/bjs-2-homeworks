08acbad407382ecd75386a2a8bf8f11432b4858e
Roksana Rakhmanova <rahmanovaroksana45@gmail.com>
2026-08-29 16:08:29 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,64 @@
-﻿
+﻿'use strict';
+
+function parseCount(value) {
+  const result = Number.parseFloat(value);
+
+  if (Number.isNaN(result)) {
+    throw new Error('Невалидное значение');
+  }
+
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const halfPerimeter = this.perimeter / 2;
+
+    const area = Math.sqrt(
+      halfPerimeter *
+        (halfPerimeter - this.a) *
+        (halfPerimeter - this.b) *
+        (halfPerimeter - this.c)
+    );
+
+    return Number(area.toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      },
+
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+    };
+  }
+}
